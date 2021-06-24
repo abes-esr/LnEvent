f77ec37c328fc30e389fec9d6e9b6f6f0c2da596
kiterfoo <pouilloux@abes.fr>
2021-06-24 11:31:55 +0200

    ajout url autorisé

--- src/main/java/fr/abes/lnevent/LneventApplication.java
+++ src/main/java/fr/abes/lnevent/LneventApplication.java
@@ -26,8 +26,9 @@ public class LneventApplication {
         config.setAllowCredentials(true);
         // *** URL below needs to match the Vue client URL and port ***
         config.setAllowedOrigins(Collections.singletonList("https://acces-docker.licencesnationales.fr"));
-        config.setAllowedOrigins(Collections.singletonList("https://robinier1-test.v202.abes.fr:8081"));
-        config.setAllowedOrigins(Collections.singletonList("https://robinier1-test.v202.abes.fr:8000"));
+        config.setAllowedOrigins(Collections.singletonList("http://raiponce1-test.v202.abes.fr"));
+        config.setAllowedOrigins(Collections.singletonList("http//robinier1-test.v202.abes.fr:8081"));
+        config.setAllowedOrigins(Collections.singletonList("http://robinier1-test.v202.abes.fr:8000"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
